75b72da912e3f95e00ad416df30ffba771060cbb
Hussein Tahmazov <tahmazovhussejn@gmail.com>
2026-01-05 13:06:00 +0200

    Lisasin funktsiooni naitaTabel ja lühendasin seda.

--- content/valimised/funktsionid.php
+++ content/valimised/funktsionid.php
@@ -8,3 +8,18 @@ function lisapunkt($id){
     $paring->bind_param("i", $id);
     $paring->execute();
 }
+
+function naitaTabel($id){
+    global $conn;
+    $paring=$conn->prepare("
+    select id, president, pilt, punktid, lisamiaeg, kommentaarid from valimised where avalik=1");
+    $paring->bind_result($id, $president, $pilt, $punktid, $lisamisaeg, $kommentaarid);
+    $paring->execute();
+    while ($paring->fetch()) {
+        echo "<tr>";
+        echo "<td>" . $president . "</td>";
+        echo "<td>$punktid</td>";
+        echo "<td><a href='?lisa1punkt=$id'> +1 punkt</a></td>";
+        echo "</tr>";
+    }
+}
